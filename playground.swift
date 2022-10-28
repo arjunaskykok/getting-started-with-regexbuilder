cf22ce160244577390c6968bd70f82a735e2e87e
Arjuna Sky Kok <32124593+arjunaskykok@users.noreply.github.com>
2022-10-28 16:31:40 +0700

    Create playground.swift

---
+++ playground.swift
@@ -0,0 +1,120 @@
+import UIKit
+import RegexBuilder
+
+
+let regex = Regex {
+    OneOrMore(.digit)
+    "@"
+    OneOrMore(.word)
+}
+
+let regex2 = Regex {
+    Optionally(.digit)
+    "@"
+    OneOrMore(.word)
+}
+
+let regex3 = Regex {
+    Repeat(4...6) {
+        .digit
+    }
+    "@"
+    OneOrMore(.word)
+}
+
+let text = "Writer/Arjuna Sky Kok/$1,000/December 4, 2022"
+let text2 = "Illustrator/Karen O'Reilly/$350/November 30, 2022"
+
+let job = Regex {
+    ChoiceOf {
+        "Writer"
+        "Illustrator"
+    }
+}
+
+if let jobMatch = text.firstMatch(of: job) {
+    let (wholeMatch) = jobMatch.output
+    print(wholeMatch)
+}
+
+let name = Regex {
+    OneOrMore(
+        ChoiceOf {
+            CharacterClass(.word)
+            CharacterClass(.whitespace)
+            "'"
+        }
+    )
+}
+
+if let nameMatch = "Karen O'Reilly".firstMatch(of: name) {
+    let (wholeMatch) = nameMatch.output
+    print(wholeMatch)
+}
+
+let USlocale = Locale(identifier: "en_US")
+let payment = Regex {
+    One(.localizedCurrency(code: "USD", locale: USlocale))
+}
+
+if let paymentMatch = text.firstMatch(of: payment) {
+    let (wholeMatch) = paymentMatch.output
+    print(wholeMatch)
+}
+
+let date = Regex {
+    One(.date(.long, locale: USlocale, timeZone: .gmt))
+}
+
+if let dateMatch = text.firstMatch(of: date) {
+    let (wholeMatch) = dateMatch.output
+    print(wholeMatch)
+}
+
+let separator = Regex { "/" }
+let regexCode = Regex {
+    job
+    separator
+    name
+    separator
+    payment
+    separator
+    date
+}
+
+if let match = text.firstMatch(of: regexCode) {
+    let (wholeMatch) = match.output
+    print(wholeMatch)
+}
+
+if let match2 = text2.firstMatch(of: regexCode) {
+    let (wholeMatch) = match2.output
+    print(wholeMatch)
+}
+
+let regexCodeWithCapture = Regex {
+    Capture {
+        job
+    }
+    separator
+    Capture {
+        name
+    }
+    separator
+    Capture {
+        payment
+    }
+    separator
+    Capture {
+        date
+    }
+}
+
+if let matchWithCapture = text.firstMatch(of: regexCodeWithCapture) {
+    let (wholeMatch) = matchWithCapture.output
+    print(wholeMatch.0)
+    print(wholeMatch.1)
+    print(wholeMatch.2)
+    print(wholeMatch.3)
+    print(wholeMatch.4)
+}
